59a5b9802205584074a160ca54e8ea74f091a4a1
HerainYayo <yangzhonghao001211@gmail.com>
2024-11-11 02:20:45 +1100

    fix cors 3

--- src/index.ts
+++ src/index.ts
@@ -23,16 +23,10 @@ app.use(express.json());
 
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
+  origin: ['http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
 
 app.use(session({
     secret: 'mySecret',
