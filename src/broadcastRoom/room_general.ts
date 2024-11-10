4a1e0dc6f937218c89263abd68eb8cbd6670b6d3
HerainYayo <yangzhonghao001211@gmail.com>
2024-11-11 02:59:39 +1100

    test api token

--- src/broadcastRoom/room_general.ts
+++ src/broadcastRoom/room_general.ts
@@ -92,7 +92,8 @@ function register(app: Application, moduleManagerInstance: ModuleManager) {
       // }
 
       res.json({
-        'id': broadCastId
+        'id': broadCastId,
+        'token': moduleManagerInstance.authManager.getAccessToken()
       });
     } catch (err: any) {
       res.status(err.response?.status || 500).json({ message: err.message });
