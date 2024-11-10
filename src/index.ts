50687384af6b340e2b7b7548679a6b7d97d69dd0
HerainYayo <yangzhonghao001211@gmail.com>
2024-11-11 02:08:36 +1100

    fix cors 2

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,13 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 app.use(session({
     secret: 'mySecret',
     resave: false,
