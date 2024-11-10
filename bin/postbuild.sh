7f490c9ea0bf1f738ea3fd29027f7b8c310ed1ae
HerainYayo <yangzhonghao001211@gmail.com>
2024-11-11 00:16:41 +1100

    cp env

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -10,3 +10,5 @@ cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r public ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+cp .env ./.amplify-hosting/.env
