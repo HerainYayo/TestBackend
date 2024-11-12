e07a9775d7087213bb283de7b4e245d49794bad7
HerainYayo <yangzhonghao001211@gmail.com>
2024-11-12 16:03:28 +1100

    cors

--- src/core/mongoConnection.ts
+++ src/core/mongoConnection.ts
@@ -1,8 +1,8 @@
-import { MongoClient, Collection } from 'mongodb'
+import { MongoClient, Collection } from "mongodb";
 
 const uri = process.env.DB_URL as string;
-console.log('db string', uri);
-console.log('env', process.env)
+console.log("db string", uri);
+console.log("env", process.env);
 const client = new MongoClient(uri);
 
 const dictionary: { [key: string]: Collection } = {};
@@ -10,34 +10,37 @@ const dictionary: { [key: string]: Collection } = {};
 const tail = ([dbs, cols, callback, ...xs]: any[]) => xs;
 
 async function runMongoFunction(
-    dbString: string,
-    collectionString: string,
-    callback: (collection: Collection, ...args: any[]) => Promise<any>,
-    ...args: any[]
+  dbString: string,
+  collectionString: string,
+  callback: (collection: Collection, ...args: any[]) => Promise<any>,
+  ...args: any[]
 ): Promise<any> {
-    let results;
-    try {
-        console.log("start connect to db")
-        await client.connect();
-        console.log("connect finish")
-        const dbCollectionString = `${dbString}+${collectionString}`;
-        let collection: Collection;
+  let results;
+  try {
+    console.log("start connect to db");
+    await client.connect();
+    console.log("connect finish");
+    const dbCollectionString = `${dbString}+${collectionString}`;
+    let collection: Collection;
 
-        if (!dictionary[dbCollectionString]) {
-            const database = client.db(dbString);
-            collection = database.collection(collectionString);
-            dictionary[dbCollectionString] = collection;
-        } else {
-            collection = dictionary[dbCollectionString];
-        }
-
-        results = await callback(collection, ...args);
-
-    } catch (e) {
-        console.error(e);
+    if (!dictionary[dbCollectionString]) {
+      const database = client.db(dbString);
+      collection = database.collection(collectionString);
+      dictionary[dbCollectionString] = collection;
+    } else {
+      collection = dictionary[dbCollectionString];
     }
 
-    return results;
+    results = await callback(collection, ...args);
+  } catch (e) {
+    console.error(e);
+  }
+
+  return results;
 }
+
+process.on("exit", () => {
+  client.close();
+});
 
 export { runMongoFunction };
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import axios from "axios";
-import session from 'express-session';
-import dotenv from 'dotenv';
+import session from "express-session";
+import dotenv from "dotenv";
 dotenv.config({
-  path: __dirname + '/.env.local'
+  path: __dirname + "/.env.local",
 });
 
-console.log(__dirname)
+console.log(__dirname);
 
-import {moduleManagerInstance} from "./core/moduleManager";
+import { moduleManagerInstance } from "./core/moduleManager";
 
-import { register as register_auth } from './auth/auth_general';
-import { register as register_broadcast } from './broadcastRoom/room_general';
+import { register as register_auth } from "./auth/auth_general";
+import { register as register_broadcast } from "./broadcastRoom/room_general";
 
 const app: express.Application = express();
 const port = process.env.PORT || 3000;
@@ -22,18 +22,25 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(helmet());
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://main.d367p4ccixpyms.amplifyapp.com",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.use(session({
-    secret: 'mySecret',
+app.use(
+  session({
+    secret: "mySecret",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
-  }));
+    cookie: { secure: false },
+  })
+);
 
 app.use(express.text());
 
@@ -45,31 +52,43 @@ register_auth(app, moduleManagerInstance);
 register_broadcast(app, moduleManagerInstance);
 
 // Homepage
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send("Hello World!");
 });
 
 // GET
-app.get('/get', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-get-header", "get-header-value").send("get-response-from-compute");
+app.get("/get", (req: express.Request, res: express.Response) => {
+  res
+    .status(200)
+    .header("x-get-header", "get-header-value")
+    .send("get-response-from-compute");
 });
 
 //POST
-app.post('/post', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-post-header", "post-header-value").send(req.body.toString());
+app.post("/post", (req: express.Request, res: express.Response) => {
+  res
+    .status(200)
+    .header("x-post-header", "post-header-value")
+    .send(req.body.toString());
 });
 
 //PUT
-app.put('/put', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-put-header", "put-header-value").send(req.body.toString());
+app.put("/put", (req: express.Request, res: express.Response) => {
+  res
+    .status(200)
+    .header("x-put-header", "put-header-value")
+    .send(req.body.toString());
 });
 
 //PATCH
-app.patch('/patch', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-patch-header", "patch-header-value").send(req.body.toString());
+app.patch("/patch", (req: express.Request, res: express.Response) => {
+  res
+    .status(200)
+    .header("x-patch-header", "patch-header-value")
+    .send(req.body.toString());
 });
 
 // Delete
-app.delete('/delete', (req: express.Request, res: express.Response) => {
+app.delete("/delete", (req: express.Request, res: express.Response) => {
   res.status(200).header("x-delete-header", "delete-header-value").send();
 });
