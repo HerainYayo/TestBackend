769eca4c66e013c7d8773125a0ce9bbbc3fcc3af
HerainYayo <yangzhonghao001211@gmail.com>
2024-11-12 16:22:32 +1100

    cors update

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://main.d367p4ccixpyms.amplifyapp.com",
+      "https://main.dbx8lkzfcsuib.amplifyapp.com/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
